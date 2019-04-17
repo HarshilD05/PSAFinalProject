3b9d93c175ebef6d5c49eab275d392a97ed9e2e5
NikitaGundyal <nikitagundyal13@gmail.com>
2019-04-17 17:40:02 -0400

    Added Genetic Algorithm class with evolve and crossoverPopulation methods

--- UniversityClassScheduling/src/university/timetable/scheduling/genetic/algorithm/GeneticAlgorithm.java
+++ UniversityClassScheduling/src/university/timetable/scheduling/genetic/algorithm/GeneticAlgorithm.java
@@ -1,5 +1,31 @@
 package university.timetable.scheduling.genetic.algorithm;
+import java.util.stream.IntStream;
+
 
 public class GeneticAlgorithm {
+	
+	private Data data;
+
+	public GeneticAlgorithm(Data data) {
+		this.data = data;
+	}
+	
+	public Population evolve(Population population) {
+		return mutatePopulation(crossOverPopulation(population));
+	}
+	
+	public Population crossOverPopulation(Population population) {
+		Population crossOverPopulation=new Population(population.getSchedules().size(),data);
+		IntStream.range(0,Driver.NUM_OF_ELITE_SCHEDULES).forEach(x -> crossOverPopulation.getSchedules().set(x,population.getSchedules().get(x)));
+		
+		IntStream.range(Driver.NUM_OF_ELITE_SCHEDULES, population.getSchedules().size()).forEach(x -> {
+			if(Driver.CROSSOVER_RATE > Math.random()) {
+				Schedule schedule1=selectClassPopulation(population).sortByFitness().getSchedules().get(0);
+				Schedule schedule2=selectClassPopulation(population).sortByFitness().getSchedules().get(0);
+				crossOverPopulation.getSchedules().set(x,crossOverSchedule(schedule1, schedule2));
+			}else crossOverPopulation.getSchedules().set(x,population.getSchedules().get(x));
+		});
+		return crossOverPopulation;
+	}
 
 }
