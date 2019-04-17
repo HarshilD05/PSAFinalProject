f4c4c77b17bd0abb08f1bbd6f6fa7ae2d62b6153
Harshil Duahat <dudhat.h@husky.neu.edu>
2019-04-17 13:33:53 -0400

    Pojo added

--- UniversityClassScheduling/src/university/timetable/scheduling/domain/Class.java
+++ UniversityClassScheduling/src/university/timetable/scheduling/domain/Class.java
@@ -1,5 +1,70 @@
 package university.timetable.scheduling.domain;
 
-public class Class {
+import com.neu.edu.doamin.Course;
+import com.neu.edu.doamin.Department;
+import com.neu.edu.doamin.Instructor;
+import com.neu.edu.doamin.MeetingTime;
+import com.neu.edu.doamin.Room;
 
+public class Class {
+	private int classId;
+	private Department department;
+	private Course course;
+	private Instructor instructor;
+	private MeetingTime meetingTime;
+	private Room room;
+	
+	
+	public Class(int id, Department dept, Course course) {
+		this.classId = id;
+		this.department = dept;
+		this.course = course;
+	}
+
+
+	public void setInstructor(Instructor instructor) {
+		this.instructor = instructor;
+	}
+
+	public void setMeetingTime(MeetingTime meetingTime) {
+		this.meetingTime = meetingTime;
+	}
+
+	public void setRoom(Room room) {
+		this.room = room;
+	}
+
+
+	public int getClassId() {
+		return classId;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+
+	public Course getCourse() {
+		return course;
+	}
+
+
+	public Instructor getInstructor() {
+		return instructor;
+	}
+
+
+	public MeetingTime getMeetingTime() {
+		return meetingTime;
+	}
+
+
+	public Room getRoom() {
+		return room;
+	}
+	
+	public String toString() {
+		return department.getName()+", "+ course.getNumber()+", "+room.getRoomnumber()+", "+ instructor.getName()+", "+meetingTime.getId();
+		
+	}
 }
--- UniversityClassScheduling/src/university/timetable/scheduling/pojo/Course.java
+++ UniversityClassScheduling/src/university/timetable/scheduling/pojo/Course.java
@@ -1,5 +1,39 @@
 package university.timetable.scheduling.pojo;
 
-public class Course {
+import java.util.ArrayList;
 
+public class Course {
+	private String name =null;
+	private String number=null;
+	private int maxNumberOfStudents;
+	private ArrayList<Instructor> instructors;
+	
+	public Course(String number, String name, int maxNumberOfStudents, ArrayList<Instructor> instructors) {
+		
+		this.name = name;
+		this.number = number;
+		this.maxNumberOfStudents = maxNumberOfStudents;
+		this.instructors = instructors;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public int getMaxNumberOfStudents() {
+		return maxNumberOfStudents;
+	}
+
+	public ArrayList<Instructor> getInstructors() {
+		return instructors;
+	}
+	
+	public String toString() {
+		return name;
+		
+	}
 }
